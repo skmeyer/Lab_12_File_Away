6feaae19a340dd7e9ab4982ca83da31493f4c115
skmeyer <sarahbutwritingeditioN@gmail.com>
2025-04-08 11:19:36 -0400

    Commit

--- src/FileInspector.java
+++ src/FileInspector.java
@@ -1,7 +1,26 @@
+import javax.swing.*;
+import java.io.File;
+import java.nio.file.Path;
+
 public class FileInspector
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
+        JFileChooser inspector = new JFileChooser();
+        File selectedFile;
+        String rec = "";
+
+        try
+        {
+            File workingDirectory = new File(System.getProperty("user.dir"));
+            inspector.setCurrentDirectory(workingDirectory);
+
+            if (inspector.showOpenDialog(null)) == JFileChooser.APPROVE_OPTION)
+            {
+                selectedFile = inspector.getSelectedFile();
+                Path file = selectedFile.toPath();
+
+            }
+        }
     }
 }
