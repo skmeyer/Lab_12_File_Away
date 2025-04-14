72fcece28fe9958c9fa550d46b537f848a75fb48
skmeyer <sarahbutwritingeditioN@gmail.com>
2025-04-14 17:27:05 -0400

    Commit

--- src/FileInspector.java
+++ src/FileInspector.java
@@ -1,31 +1,58 @@
 import javax.swing.*;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
+
+import static java.nio.file.StandardOpenOption.CREATE;
 
 public class FileInspector
 {
     public static void main(String[] args)
     {
-        JFileChooser inspector = new JFileChooser();
+        JFileChooser chooser = new JFileChooser();
         File selectedFile;
         String rec = "";
 
         try
         {
             File workingDirectory = new File(System.getProperty("user.dir"));
-            inspector.setCurrentDirectory(workingDirectory);
+            chooser.setCurrentDirectory(workingDirectory);
 
-            if (inspector.showDialog(null) == JFileChooser.APPROVE_OPTION)
+
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
             {
-                selectedFile = inspector.getSelectedFile();
+                selectedFile = chooser.getSelectedFile();
                 Path file = selectedFile.toPath();
+                InputStream in = new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 
+                int line = 0;
+                while(reader.ready())
+                {
+                    rec = reader.readLine();
+                    String [] word = rec.split(" ");
+                    int wordCount = word.length + wordCount;
+                    line++;
+                    System.out.println("[" + line + "] " + rec);
+                }
+                reader.close();
+                System.out.println("The file had " + line + " lines. The file had " + wordCount + " characters." );
+                System.out.println("Finished reading the file!");
+
+            }
+            else
+            {
+                System.out.println("No file selected!");
             }
         }
         catch (FileNotFoundException e)
         {
-            
+            System.out.println("No file found! Restart the program and try again.");
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
         }
     }
 }
