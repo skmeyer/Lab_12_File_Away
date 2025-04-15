cf132fcb086fd2b1a20d2dd0a543bf69d98f6a52
skmeyer <sarahbutwritingeditioN@gmail.com>
2025-04-15 15:01:13 -0400

    Complete

--- src/DataSaver.java
+++ src/DataSaver.java
@@ -1,8 +1,11 @@
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import static java.nio.file.StandardOpenOption.CREATE;
 
 public class DataSaver
 {
@@ -31,6 +34,26 @@ public class DataSaver
         String fileName = in.nextLine();
         File workingDirectory = new File(System.getProperty("user.dir"));
         Path file = Paths.get(workingDirectory.getPath() + "\\src\\" + fileName + ".csv");
+        
+        try
+        {
+            OutputStream out = new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
+
+            for(String recs : data)
+            {
+                writer.write(recs, 0, recs.length());
+                writer.newLine();
+                
+            }
+            writer.close();
+            System.out.println("Finished writing your file! Check the src directory of the program.");
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+        
         
         
 
