1f18231f12f1b75ae804d57da3be8eb59da4a7ec
skmeyer <sarahbutwritingeditioN@gmail.com>
2025-04-15 12:10:32 -0400

    Commit

--- src/DataSaver.java
+++ src/DataSaver.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -5,10 +8,30 @@ public class DataSaver
 {
     public static void main(String[] args)
     {
+        boolean done = false;
         Scanner in = new Scanner(System.in);
-        System.out.println("Use the following format: [FirstName, LastName, ID, Email, BirthYear]");
         ArrayList <String> data = new ArrayList<>();
-        String record = SafeInput.getRegExString(in,"Enter your record", "^\\d{},\\d{},\\d{6},\\d{},\\d{4}$");
+        do
+        {
+            System.out.println("Use the following format: [FirstName, LastName, ID, Email, BirthYear]");
+            String record = SafeInput.getRegExString(in,"Enter your record", "^[A-Za-z]*,\\s[A-Za-z]*,\\s\\d{6},\\s[A-Za-z0-9.@-_~+]*,\\s\\d{4}$");
+            data.add(record);
+            boolean continueYN = SafeInput.getYNConfirm(in, "Would you like to continue entering records?");
+            if(!continueYN)
+            {
+                done = true;
+            }
+        }while(!done);
+        for(int x = 0; x < data.size(); x++)
+        {
+            int itemNum = x + 1;
+            System.out.println("[" + itemNum + "] " + data.get(x));
+        }
+        System.out.println("Enter the name for your file: ");
+        String fileName = in.nextLine();
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        Path file = Paths.get(workingDirectory.getPath() + "\\src\\" + fileName + ".csv");
+        
         
 
     }
